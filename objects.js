3093a7c2367988cafc27be441e392195696c0354
david ritchey <ritchey0713@gmail.com>
2018-03-04 19:37:49 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,3 +6,9 @@ function updatePlaylist (playlist,journey, faithfully){
   playlist["journey"] = "faithfully"
   return playlist;
 }
+
+function removeFromPlaylist(playlist, journey){
+  delete playlist.journey;
+  return playlist;
+}
+  
